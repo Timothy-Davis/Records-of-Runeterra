1108180d38978add3d7a68fcf7548e7b175013a0
Timothy-Davis <dtimmy690@gmail.com>
2019-11-14 09:04:48 -0500

    Created a function to parse Data Dragon and insert cards to the database

--- src/json_parser.py
+++ src/json_parser.py
@@ -1,1 +1,11 @@
 import json
+from src import dbUpdate
+
+
+# THe following function will add all the cards from the Data Dragon to the database.
+def add_all_cards():
+    card_json = open('C:/datadragon-set1-en_us/en_us/data/set1-en_us.json', encoding='UTF-8')
+    card_data = json.load(card_json)
+
+    for i in range(len(card_data)):
+        dbUpdate.add_card(card_data[i]['cardCode'], card_data[i]['name'], card_data[i]['type'])
