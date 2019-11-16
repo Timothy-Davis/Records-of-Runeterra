caf45ef6e54ad3b5b34b3415cc50ece21c491188
Timothy-Davis <dtimmy690@gmail.com>
2019-11-16 13:21:23 -0500

    Made more explicit debugging print statements
    Also adjusted sleep time and reformatted some code.

--- src/json_parser.py
+++ src/json_parser.py
@@ -20,30 +20,37 @@ def check_game_state():
     deck_code = None
 
     while jsonFetcher.poll_active_deck()['DeckCode'] is None:
-        print("not in game")
-        time.sleep(10)
+        print("STATUS: ", "not in game")
+        time.sleep(5)
 
     while jsonFetcher.poll_active_deck()['DeckCode'] is not None:
-        print("in game")
+        print("STATUS: ", "in game")
+
         summoner = jsonFetcher.poll_positional_rectangles()['PlayerName']
         deck_code = jsonFetcher.poll_active_deck()['DeckCode']
+
         if dbUpdate.search_decks(deck_code) is True:
             dbUpdate.add_deck(deck_code)
+
         if dbUpdate.search_users(summoner) is True:
             dbUpdate.add_user(summoner)
-        print(summoner, deck_code)
+
+        print("SUMMONER: ", summoner, "DECK: ", deck_code)
+
         while jsonFetcher.poll_active_deck()['DeckCode'] is not None:
-            time.sleep(1)
+            time.sleep(0.1)
 
     if deck_code is not None and summoner is not None:
-        print("post game")
+        print("STATUS: ", "post game")
+
         game_result = jsonFetcher.poll_game_result()['LocalPlayerWon']
         deck = decode.decode_deck(deck_code)
 
+        print("STATUS: ", "updating database")
         if game_result:
             for i in range(len(deck)):
                 dbUpdate.update_card(deck[i][2:], 'games_won')
-            dbUpdate.update_user(summoner, "wins")
+            dbUpdate.update_user(summoner, "total_wins")
             dbUpdate.update_deck(deck_code, "wins")
 
         else:
@@ -51,6 +58,8 @@ def check_game_state():
                 dbUpdate.update_card(deck[i][2:], 'games_played')
             dbUpdate.update_user(summoner, "total_games")
             dbUpdate.update_deck(deck_code, "total_games")
+
+        check_game_state()
 
 
 if __name__ == "__main__":
